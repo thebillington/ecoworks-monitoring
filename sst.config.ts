fb7df9ded2689352a684245c55187094d212db75
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-09-04 20:07:24 +0100

    Initial SST setup (broken)

---
+++ sst.config.ts
@@ -0,0 +1,18 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "ecoworks-monitoring",
+      home: "aws",
+      providers: {
+        aws: {
+          region: "eu-west-2",
+        }
+      }
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("Ecoworks Monitoring");
+  },
+});
