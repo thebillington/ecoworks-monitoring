b558d4d00548c2c3d904d1e2ad1e2c6da955c8f6
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-09-16 09:09:57 +0100

    Remove unneeded dialog

--- src/components/dashboard/project/attendance-sheet.tsx
+++ src/components/dashboard/project/attendance-sheet.tsx
@@ -80,12 +80,6 @@ export default async function AttendanceSheetComponent(
             <div className="flex flex-col w-full text-center mt-6">
                 <button className="w-full mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
             </div>
-            <dialog id="user-details" className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click outside to close</p>
-                </div>
-            </dialog>
         </>
     )
 }
